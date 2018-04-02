917caa9d46fab7c2bf49528e4cc16fb905bcf44d
Aminata <ami.sy@hotmail.fr>
2018-04-02 19:00:37 +0200

    ajout dossier

---
+++ app.js
@@ -0,0 +1,61 @@
+function Calculator()
+{
+  that        = this,
+  this.field  = "input#number",
+  this.button = "#body .buttons",
+  this.init   = false,
+
+  this.run = function()
+  {
+
+    $(this.button).click(function() {
+      const value = $(this).html();
+
+      if (that.init == false)
+      {
+        $(that.field).val("");
+        that.init = true;
+      }
+      if (value != "=")
+         $(that.field).val($(that.field).val() + value);
+
+      that.dispatcher(value);
+    });
+  },
+  this.dispatcher = function(value)
+  {
+     if ($(this.field).val().indexOf("/") != -1)
+        this.operation(value, "/");
+     if ($(this.field).val().indexOf("*") != -1)
+        this.operation(value, "*");
+     if ($(this.field).val().indexOf("-") != -1)
+        this.operation(value, "-");
+     if ($(this.field).val().indexOf("=") != -1)
+        this.operation(value, "+");
+  },
+
+  this.operation = function(value, symbol)
+  {
+     const numbers = $(this.field).val().split(symbol),
+         result;
+
+     if (symbol == "/")
+         result = numbers[0] / numbers[1];
+     if (symbol == "*")
+         result = numbers[0] * numbers[1];
+     if (symbol == "-")
+         result = numbers[0] - numbers[1];
+     if (symbol == "+")
+         result = parseFloat(numbers[0]) + parseFloat(numbers[1]);
+
+     result = Math.round((result) * 100) / 100;
+
+     if (numbers.lengt > 1)
+     {
+       if (value == "=")
+          $(this.field).val(result);
+       else if (numbers.length > 2)
+          $(this.field).val(result + symbol);
+     }
+  };
+}
